4c3e393604ae536aab1e56980e87508da9528e62
Unknown <eliocampitelli@gmail.com>
2019-03-12 16:02:02 -0300

    Adds package documentation

---
+++ R/ggnewscale-package.R
@@ -0,0 +1,8 @@
+#' @section Usage:
+#'
+#' To have more than one colour or fill scale in a plot using ggplot2, you add a
+#' new scale slot with `new_scale()` and the following geoms will use a
+#' different scale than the previous ones.
+#'
+#' @keywords internal
+"_PACKAGE"
