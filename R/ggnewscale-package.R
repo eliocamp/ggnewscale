8978213d2eccbaf476582dfc49a372bc4682f6b6
Elio Campitelli <eliocampitelli@gmail.com>
2019-10-01 17:29:05 -0300

    Updates documentation

--- R/ggnewscale-package.R
+++ R/ggnewscale-package.R
@@ -1,8 +1,14 @@
 #' @section Usage:
 #'
 #' To have more than one colour or fill scale in a plot using ggplot2, you add a
-#' new scale slot with `new_scale()` and the following geoms will use a
+#' new scale slot with [new_scale] and the following geoms will use a
 #' different scale than the previous ones.
 #'
 #' @keywords internal
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
