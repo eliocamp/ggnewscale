0903266e9a5c1880e6bda1f460ad75890aad5e92
Travis CI User <travis@example.org>
2019-10-01 20:34:23 +0000

    Built site for ggnewscale: 0.4.0.9000@39745c6

---
+++ reference/figures/makes_ring.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+
+ggplot(expand.grid(x = 1:20, y = 1) ) +
+  geom_tile(aes(x, y, fill = cos(x/20*2*pi))) +
+  scale_y_continuous(limits = c(-2, 2)) +
+  coord_polar() +
+  theme_void()  -> base
+
+
+options <- c("magma", "inferno", "plasma", "viridis", "cividis")
+
+
+lapply(options, function(o) {
+
+  g <- base + scale_fill_viridis_c(guide = "none", option = o)
+  ggsave(paste0("man/figures/", o, ".svg"), plot = g)
+
+}) -> n
