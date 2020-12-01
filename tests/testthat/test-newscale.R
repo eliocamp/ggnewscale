f171d778a25a1c6e43bbe29fc41fda27b7672d3e
Elio Campitelli <eliocampitelli@gmail.com>
2020-12-01 17:40:06 -0300

    Uses more robust inheritance

--- tests/testthat/test-newscale.R
+++ tests/testthat/test-newscale.R
@@ -47,6 +47,7 @@ test_that("doesn't do partial matching", {
 
 
 test_that("stats with custom `setup_data`", {
+  # from https://github.com/eliocamp/ggnewscale/issues/27
   # Manipulate `setup_data()` of `StatYdensity` and store the object as `StatYdensity2`.
   StatYdensity2 <- ggplot2::ggproto("StatYdensity2", StatYdensity,
                                     setup_data = function (data, params) {
@@ -72,4 +73,26 @@ test_that("stats with custom `setup_data`", {
     geom_point(aes(x, y, fill = fill), shape = 21, inherit.aes = F)
   expect_error(print(g), NA)
 
+})
+
+
+test_that("works with many layers", {
+  # from https://github.com/eliocamp/ggnewscale/issues/29
+  data <- expand.grid(y = 1:4, x = 1:4)
+  data$z <- c("a", "b")
+
+
+  layer <- function(number) {
+    list(new_scale_fill(),
+         geom_tile(data = ~.x[.x$x == number, ], aes(fill = z)),
+         scale_fill_brewer(name = number, palette = number)
+    )
+  }
+  g <- ggplot(data, aes(x, y)) +
+    layer(1) +
+    layer(2) +
+    layer(3) +
+    layer(4)
+
+  vdiffr::expect_doppelganger("many_layers", g)
 })
